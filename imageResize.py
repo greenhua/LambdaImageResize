4c1181d752fbe6194bc5e9ef3759bedd5d03ac47
greenhua <greenh@gmail.com>
2020-05-15 00:42:38 +0300

    Update imageResize.py

--- imageResize.py
+++ imageResize.py
@@ -11,16 +11,27 @@ from io import BytesIO
 
 def lambda_handler(event, context):
     resource = boto3.resource('s3')
-    bucket = resource.Bucket('buydo-images')
+    bucket = resource.Bucket('my-images')
 
     path = event['path']
-    
+    print("path = " + path)
+    # path = "/images/crop/315/men-1.png"
     # path = "/images/crop/200/46/04/28/460428d8b81f36a641f75d656dc4efc7.jpg"
     # path = "/images/resize/400/4a/45/87/4a45872e111aa429b3cf08a3b0205440.jpg"
+    # path = "/video/d5/5a/fa/d55afaea2f4a85330632eb4eb66529f8.mp4"
     fname = os.path.basename(path)
     content_type = "image/png"
+    format = "png"
     if re.match(r".+jpg", fname)  or re.match(r".+jpeg", fname):
         content_type = "image/jpeg"
+        format = "jpeg"
+
+    if re.match(r".+mpeg", fname) or re.match(r".+MPEG", fname):
+        content_type = "video/mpeg"
+
+    if re.match(r".+mp4", fname) or re.match(r".+MP4", fname):
+        content_type = "video/mp4"
+
 
     response = {
         "statusCode": 200,
@@ -31,6 +42,16 @@ def lambda_handler(event, context):
             "Access-Control-Allow-Origin": "*"
         }
     }
+
+
+    if re.match(r".+video", path):
+        image_object = bucket.Object("data" + path)
+        string = image_object.get()['Body'].read()
+        response['body'] = base64.b64encode(string).decode("utf-8")
+        print("video")
+        return response;
+
+
 
     if re.match(r".+resize", path):
         fname = os.path.basename(path)
@@ -43,10 +64,9 @@ def lambda_handler(event, context):
         new_height = int(size*height/width)
         new_width = size
         im1 = im1.resize((new_width, new_height), Image.ANTIALIAS)
-        im1.save("/tmp/" + fname)
-        f = open("/tmp/" +fname,"rb")
-        string = f.read()
-        os.remove("/tmp/" + fname)
+        with BytesIO() as output:
+            im1.save(output, format=format)
+            string = output.getvalue()
         response['body'] = base64.b64encode(string).decode("utf-8")
     elif re.match(r".+crop", path):
         fname = os.path.basename(path)
@@ -72,21 +92,19 @@ def lambda_handler(event, context):
         right_top = left_top + size
         right_bottom = left_bottom + size
         im1 = im1.crop((left_top, left_bottom, right_top, right_bottom))
-        im1.save("/tmp/" + fname)
-        # im1.save(fname)
+        with BytesIO() as output:
+            im1.save(output, format=format)
+            string = output.getvalue()
 
-        f = open("/tmp/" +fname,"rb")
-        string = f.read()
-        os.remove("/tmp/" + fname)
         response['body'] = base64.b64encode(string).decode("utf-8")
     else:
-        print("path=data" + path)
+        print("key = data" + path+"|")
         image_object = bucket.Object("data" + path)
         string = image_object.get()['Body'].read()
         response['body'] = base64.b64encode(string).decode("utf-8")
-
-    #response['body'] = json.dumps(event['path'])
-#    print(response)
+        #response['body'] = json.dumps(event['path'])
+        print("last debug")
+        
     return response
 
 
